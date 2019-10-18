b3ae76dd077024ab390dd4052a1c0dc56728d607
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-10-17 21:04:37 -0700

    Change precision of immutable snapshot to YYYYMMddHHmmss

--- src/main/groovy/nebula/plugin/release/git/opinion/TimestampUtil.groovy
+++ src/main/groovy/nebula/plugin/release/git/opinion/TimestampUtil.groovy
@@ -23,7 +23,7 @@ import java.time.format.DateTimeFormatter
 class TimestampUtil {
 
     private static DateTimeFormatter timestampDateFormat = DateTimeFormatter
-            .ofPattern("YYYYMMddHHmm")
+            .ofPattern("YYYYMMddHHmmss")
             .withZone(ZoneOffset.UTC)
 
     static String getUTCFormattedTimestamp() {
