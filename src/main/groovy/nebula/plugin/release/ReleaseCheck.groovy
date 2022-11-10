5db0bfbdc741cf9a668abcce34a275093f5c2495
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2022-11-09 19:19:02 -0800

    Add missing task annotations

--- src/main/groovy/nebula/plugin/release/ReleaseCheck.groovy
+++ src/main/groovy/nebula/plugin/release/ReleaseCheck.groovy
@@ -19,7 +19,9 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
 
+@DisableCachingByDefault
 class ReleaseCheck extends DefaultTask {
     @Input
     String branchName
